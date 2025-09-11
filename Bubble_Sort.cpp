74011052309cbbdce66d4fee458b1eb5f5330ed4
Sachin M <34841998+SachinLearns@users.noreply.github.com>
2025-09-11 06:41:55 +0530

    Implement Bubble Sort algorithm in C++

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Flag to check if any swapping happens
+        bool swapped = false;
+
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap adjacent elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = true;
+            }
+        }
+
+        // If no elements were swapped, array is already sorted
+        if (!swapped)
+            break;
+    
+    cout<<"Step "<<i+1<<": ";
+    printArray(arr, n);
+        
+    }
+}
+
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
